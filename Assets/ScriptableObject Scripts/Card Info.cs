1426063b018f7ab9a4b3501f12b59085fc430c95
Kippy Tidwell <kct2548@rit.edu>
2020-03-27 14:19:51 -0700

    Started Basic setup

---
+++ Assets/ScriptableObject Scripts/Card Info.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardInfo : ScriptableObject
+{
+    public string Name;
+
+    public List<Keyword> Keywords;
+    public Region Region;
+
+    public int ManaCost;
+    public int Attack;
+    public int Health;
+}
---
+++ Assets/ScriptableObject Scripts/Region.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Region : ScriptableObject
+{
+    public string Name;
+    public Sprite Sprite;
+}
